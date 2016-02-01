bd2ee625f504e0a36e2770032263b116f57de9e0
Helene <helene@outlook.com>
2016-02-01 14:26:33 -0700

    minor edits

--- routes/ventures.js
+++ routes/ventures.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'JADE-Bootstrap' });
+  res.render('index', { title: '*****this page is the: /ventures page' });
 });
 router.get('/:id', function(req, res, next) {
-  res.render('index', { title: 'ventures/:id page' });
+  res.render('index', { title: '******this page is the: /ventures/:id page' });
 });
 
 
