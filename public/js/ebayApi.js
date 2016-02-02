e3c3e9b295b80635c706fbd573e3414f6a3ff4b3
april sikes <aprilsikes@gmail.com>
2016-02-01 22:10:38 -0700

    working api

---
+++ public/js/ebayApi.js
@@ -0,0 +1,20 @@
+var Ebay = require('ebay')
+
+var ebay = new Ebay({
+  app_id: 'Galvaniz-8de6-4bc1-a262-d7d776da1555'
+})
+
+
+//  make request to Ebay Finding API
+var params = {
+  'OPERATION-NAME': 'findItemsByKeywords'
+, 'keywords': 'shoes'
+}
+
+ebay.get('finding', params, function (err, data) {
+  if(err) throw err
+
+  console.log(JSON.stringify(data["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][0]["title"][0]));
+  console.log(JSON.stringify(data["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][0]["galleryURL"][0]));
+  console.log(JSON.stringify(data["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][0]["sellingStatus"][0]["currentPrice"][0]["__value__"]));
+})
