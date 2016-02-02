020f3dfdd476cf6db7ae125d9fbb9928630a8179
Patrick Dullano <PWDullano@gmail.com>
2016-02-01 21:39:21 -0700

    added affiliate url for amazon to make us moola

--- public/js/amazonApi.js
+++ public/js/amazonApi.js
@@ -1,14 +1,20 @@
 var aws = require("aws-lib");
 var prodAdv = aws.createProdAdvClient('AKIAJVTAUXOMIYDSV6JQ', 'YcCjk/BzE+r7/8xodhbSav7DaINQ25ltjQekeZcT', 'noobish-20');
 
+
 var options = {SearchIndex: "Video", Keywords: "Harry Potter - The Complete 8-Film Collection"}
 
 prodAdv.call("ItemSearch", options, function(err, result) {
-  var product = [result["Items"]["Item"][0]["ASIN"], result["Items"]["Item"][0]["ItemAttributes"]["Title"]]
-  var prices = {IdType: 'ASIN', ItemId: product[0], ResponseGroup: 'Offers'}
+var product = [
+                result["Items"]["Item"][0]["ASIN"],
+                result["Items"]["Item"][0]["ItemAttributes"]["Title"],
+                result["Items"]["Item"][1]["DetailPageURL"]
+              ]
 
+var prices = {IdType: 'ASIN', ItemId: product[0], ResponseGroup: 'Offers'}
 prodAdv.call("ItemLookup", prices, function(err, results) {
-  var lowNewPrice = results["Items"]["Item"]["OfferSummary"]["LowestNewPrice"]["FormattedPrice"]
-  console.log(product[1], lowNewPrice);
+var lowNewPrice = results["Items"]["Item"]["OfferSummary"]["LowestNewPrice"]["FormattedPrice"]
+console.log(product[1], lowNewPrice);
+console.log(product[2]);
   })
 })
