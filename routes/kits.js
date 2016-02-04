8af5fd17ce126e4be73a541ce2e19adf16448448
TonyMcKendry <TonyMcKendry@gmail.com>
2016-02-04 11:36:12 -0700

    amazon ASIN call working with form and inserting into databse

--- routes/kits.js
+++ routes/kits.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex')
+var fetchASIN = require('../public/js/amazonApi.js')
 
 function Bins(){
 return knex('bins');
@@ -12,8 +13,26 @@ return knex('kits');
 
 router.get('/ventures/:ven_id/bins/:bin_id/kits/new', function(req, res, next){
   Bins().where('id', req.params.bin_id).first().then(function(result){
-    res.render('kits/new', {title: result.title, user: req.cookies.user})
+    res.render('kits/new', {result: result, user: req.cookies.user, ven: req.params.ven_id, bin: req.params.bin_id})
   })
 })
 
+router.post('/ventures/:ven_id/bins/:bin_id/kits', function(req, res, next){
+  var asin = req.body.asin;
+  var binish = req.body.bin
+  fetchASIN(asin, function (itemData) {
+    console.log("*****DATA***");
+    console.log(itemData);
+    var obj = {}
+    obj.item = itemData.productName
+    obj.url = itemData.url
+    obj.asin = itemData.asin
+    obj.image = itemData.img
+    obj.price = itemData.lowPrice
+    obj.bin_id = binish
+    Kits().insert(obj).then(function(result){
+      res.redirect('/')
+    })
+  })
+})
 module.exports = router;
