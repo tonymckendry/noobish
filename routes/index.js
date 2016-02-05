4137364dbaa0ef45dafadd8e609d0e0094afeb14
Patrick Dullano <PWDullano@gmail.com>
2016-02-04 19:55:24 -0700

    all pages linked together

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
+
+function Ventures(){
+return knex('ventures');
+};
 
 /* GET home page. */
-// 
+//
 // router.get('/ventures/auth/signin', function(req, res, next) {
 //   res.render('auth/signin');
 // });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'this is index.js', user: req.cookies.user });
+  Ventures().then(function (results) {
+
+    console.log('this is working!! +' + results)
+    res.render('ventures/index', {ventures: results, user: req.cookies.user});
+  })
 });
 
 
